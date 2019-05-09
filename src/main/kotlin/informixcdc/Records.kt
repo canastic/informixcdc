367469cc0afb98a26a778b344ac4f09d4d7dc785
Toni Cárdenas <toni@tcardenas.me>
2019-05-09 11:39:03 +0200

    Make common Record fields abstract on sealed class.

--- src/main/kotlin/informixcdc/Records.kt
+++ src/main/kotlin/informixcdc/Records.kt
@@ -2,29 +2,32 @@ package informixcdc
 
 import java.time.Instant
 
-sealed class Record
+sealed class Record {
+    abstract val seq: Long
+    abstract val transactionID: Int
+}
 
 data class BeginTx(
-    val seq: Long,
-    val transactionID: Int,
+    override val seq: Long,
+    override val transactionID: Int,
     val startAt: Instant,
     val userID: Int
 ) : Record()
 
 data class CommitTx(
-    val seq: Long,
-    val transactionID: Int,
+    override val seq: Long,
+    override val transactionID: Int,
     val startAt: Instant
 ) : Record()
 
 data class RollbackTx(
-    val seq: Long,
-    val transactionID: Int
+    override val seq: Long,
+    override val transactionID: Int
 ) : Record()
 
 sealed class RowImage(
-    val seq: Long,
-    val transactionID: Int,
+    override val seq: Long,
+    override val transactionID: Int,
     val table: String,
     val database: String,
     val owner: String?,
@@ -68,13 +71,13 @@ class AfterUpdate(
 ) : RowImage(seq, transactionID, table, database, owner, values)
 
 data class Discard(
-    val seq: Long,
-    val transactionID: Int
+    override val seq: Long,
+    override val transactionID: Int
 ) : Record()
 
 data class Truncate(
-    val seq: Long,
-    val transactionID: Int,
+    override val seq: Long,
+    override val transactionID: Int,
     val table: String,
     val database: String,
     val owner: String?
